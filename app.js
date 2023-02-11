155f1c9108d6ba8d46a7ccbec56291f8e27fa31b
PatrickPakChiTang <61904511+PatrickPakChiTang@users.noreply.github.com>
2023-02-11 14:51:13 +0800

    line81 resources/ added

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ let webDevDiv = document.querySelector(".webDevCards")
     "Screenshot of Responsive design project", "Responsive design project");
 
     createCard(webDevCards, ["CSS", "SASS"], "sass.png", 
-    "https://PakChiTang.github.io/Techdegreeproject4/Techdegreeproject4/style_guide_v7/index.html", 
+    "https://PakChiTang.github.io/Techdegreeproject4/Techdegreeproject4/style_guide_v7/resources/index.html", 
     "Using Sass to create an awesome styled site", 
     "Screenshot of Sass Styling project", "Sass styling project");
     
